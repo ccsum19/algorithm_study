1a9e0ba1047ba961c73a7067062d7de332614949
Sumin Cho <suminyj@sogang.ac.kr>
2023-08-13 14:23:46 +0900

    0813 update

---
+++ algorithm/greedy_course.py
@@ -0,0 +1,12 @@
+def course_selection(course_list):
+    # 수업을 끝나는 순서로 정렬한다
+    sorted_list = sorted(course_list, key=lambda x: x[1])
+
+    # 가장 먼저 끝나는 수업은 무조건 듣는다
+    my_selection = [sorted_list[0]]
+
+    # 이미 선택한 수업과 안 겹치는 수업 중 가장 빨리 끝나는 수업을 고른다
+    for course in sorted_list:
+        # 마지막 수업이 끝나기 전에 새 수업이 시작하면 겹친다
+        if course[0] > my_selection[-1][1]:
+            my_selection.append(course)
